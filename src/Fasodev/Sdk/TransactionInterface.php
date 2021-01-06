4a0e15999af3e9bac862cea946a23198ac5e1d3e
faso-dev <jeromeonadja28@gmail.com>
2021-01-06 17:47:13 +0000

    feat: update the php doc

--- src/Fasodev/Sdk/TransactionInterface.php
+++ src/Fasodev/Sdk/TransactionInterface.php
@@ -9,7 +9,10 @@ declare(strict_types=1);
 
 namespace Fasodev\Sdk;
 
-
+/**
+ * Interface TransactionInterface
+ * @package Fasodev\Sdk
+ */
 interface TransactionInterface
 {
     public function processPayment();
