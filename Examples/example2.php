33f718d11728b00d61a6704463875cb154c0bb69
Anitche Chisom <anitchec.dev@gmail.com>
2021-05-20 14:19:54 +0000

    Fix typo

--- Examples/example2.php
+++ Examples/example2.php
@@ -12,7 +12,7 @@ use Fasodev\Sdk\PaymentSDK;
 require_once __DIR__ . '/../vendor/autoload.php';
 
 
-$sdk = new PaymentSDK((new OrangeMoneyAPI("username", "password", "merchantNumber", PaymentSDK::ENV_DEV))
+$sdk = new PaymentSDK((new OrangeMoneyAPI("username", "password", "merchantNumber", OrangeMoneyAPI::ENV_DEV))
     ->setAmount(1000) // Montant de la transaction
     ->setOTPCode(121212) // Code otp fourni par l'utilisateur
     ->setClientNumber(76819212)); // Le numero de client
